dff565c8f2f1e7233bf9c1add9643d2c103aea3e
tom <tomatell@gmail.com>
2015-08-17 18:48:54 +0200

    [FINISH #101402194] Mobile - change the QR Code text to JSON parsing
    added JSON parsing feature in camera.js/

--- www/js/camera.js
+++ www/js/camera.js
@@ -10,8 +10,12 @@ $(document).ready(function(){
 				"Result: " + result.text + "\n" +
 				"Format: " + result.format + "\n" +
 				"Cancelled: " + result.cancelled);
-		  //document.getElementById("frame").src = result.text; 
-		  window.location.href = result.text;
+		  //document.getElementById("frame").src = result.text;
+
+		  var obj = JSON.parse('[' + result.text + ']'); 
+		  console.log(obj[0]);
+		  alert("url:" + obj[0].url);
+		  window.location.href = obj[0].url;
 		}, 
 		function (error) {
 		  alert("Scanning failed: " + error);
